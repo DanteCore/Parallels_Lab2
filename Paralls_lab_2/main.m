bc2f7d45550a11b1cc2b3c54c3297fa858d958a5
Serhii Onopriienko <serhii.onopriienko@gmail.com>
2015-04-27 17:34:01 +0300

    Initial Commit

---
+++ Paralls_lab_2/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Paralls_lab_2
+//
+//  Created by Serhii Onopriienko on 4/27/15.
+//  Copyright (c) 2015 com.purchase.test1. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
